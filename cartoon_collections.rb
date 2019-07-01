6007c887fba62b762abcfa5f0e31ac5395bcefb4
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-01 15:39:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
 
-  array.each_with_index{|dwarf| puts " #{i}. #{dwarf}"} 
+  array.each_with_index{|dwarf, i| puts " #{i}. #{dwarf}"} 
 
 end
 
