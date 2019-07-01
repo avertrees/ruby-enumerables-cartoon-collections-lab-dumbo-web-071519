a68c9d78bbd187de4f34c5b0772ad2405e4e8af9
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-01 15:41:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(array)# code an argument here
 
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  return array.map{|el| }  
 end
 
 def long_planeteer_calls# code an argument here
