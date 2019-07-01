10ed1746eea4ea03723d26600b9458df3081ffe5
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-01 15:51:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,8 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each{
-    
+  array.map{
+    |item| cheese_types.include?{item}
   }
    array.include?{|word| }
 end
